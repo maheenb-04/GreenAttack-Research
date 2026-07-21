2fbe943119bffb1e43c5462a672c62f889d1850f
Maheen Bilal <bilalmaheen2004@gmail.com>
2026-07-21 04:24:55 -0400

    Hardcode Colab Drive dataset path

--- python_files/shared.py
+++ python_files/shared.py
@@ -26,13 +26,7 @@ from scipy import ndimage
 #tf.test.is_gpu_available()
 
 main_path = './' #path of main folder
-import os
-if os.path.exists('/content/drive/MyDrive/GreenAttack/ILSVRC2012_img_val'):
-    data_path = '/content/drive/MyDrive/GreenAttack/ILSVRC2012_img_val'
-elif os.path.exists('./DataSet/ILSVRC2012_img_val'):
-    data_path = './DataSet/ILSVRC2012_img_val'
-else:
-    data_path = '/content/CISE-QE-DBA/DataSet/ILSVRC2012_img_val'
+data_path = '/content/drive/MyDrive/GreenAttack/ILSVRC2012_img_val'
 
 images = {}
 classfiles = os.listdir(data_path)
